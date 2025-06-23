6f7c4136fcad3af7fd86326ee84fa6b62e68ae8e
Benjamin <braffour@hotmail.com>
2025-06-23 19:53:28 -0400

    Add function to retrieve secrets from Vault and integrate it into Google Ads, Facebook Ads, and CallRail sync functions

--- activities/ads_integration.go
+++ activities/ads_integration.go
@@ -1,13 +1,53 @@
 package adflowhq.activities
 
-import "context"
+import (
+    "context"
+    "os"
+    "github.com/hashicorp/vault/api"
+)
+
+func getSecretFromVault(path, key string) (string, error) {
+    vaultAddr := os.Getenv("VAULT_ADDR")
+    vaultToken := os.Getenv("VAULT_TOKEN")
+    client, err := api.NewClient(&api.Config{Address: vaultAddr})
+    if err != nil {
+        return "", err
+    }
+    client.SetToken(vaultToken)
+    secret, err := client.Logical().Read(path)
+    if err != nil || secret == nil || secret.Data == nil {
+        return "", err
+    }
+    val, ok := secret.Data[key].(string)
+    if !ok {
+        return "", err
+    }
+    return val, nil
+}
 
 func SyncGoogleAds(ctx context.Context, leadID string) error {
-    // TODO: call Google Ads API
+    apiKey, err := getSecretFromVault("secret/adflowhq/google-ads", "google_ads_key")
+    if err != nil {
+        return err
+    }
+    // TODO: Use apiKey to call Google Ads API for leadID
     return nil
 }
 
 func SyncFacebookAds(ctx context.Context, leadID string) error {
-    // TODO: call Facebook Ads API
+    apiKey, err := getSecretFromVault("secret/adflowhq/facebook-ads", "facebook_ads_key")
+    if err != nil {
+        return err
+    }
+    // TODO: Use apiKey to call Facebook Ads API for leadID
+    return nil
+}
+
+func SyncCallRail(ctx context.Context, leadID string) error {
+    apiKey, err := getSecretFromVault("secret/adflowhq/callrail", "api_key")
+    if err != nil {
+        return err
+    }
+    // TODO: Use apiKey to call CallRail API for leadID
     return nil
 }
